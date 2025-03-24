7151aa24ee2f1cef025ce1a6c86a754cacfc6752
merwil vegas <com2pa@gmail.com>
2025-03-24 10:29:33 -0400

    variables de entorno produccion o desarrollo

---
+++ config.js
@@ -0,0 +1,14 @@
+const PAGE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://backcolegio.onrender.com'
+    : 'http://localhost:5173';
+
+const MONGO_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI_PRODUC
+    : process.env.MONGO_URI_TEST;
+
+module.exports = {
+  PAGE_URL,
+  MONGO_URL,
+};
