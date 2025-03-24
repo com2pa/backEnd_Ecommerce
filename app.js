c4a10ba4bfa92058a98bd7b7b8aabb27df2b8656
merwil vegas <com2pa@gmail.com>
2025-03-24 19:24:19 -0400

    se añadieron los endpoint de user = registro , login , logout, upload , profile

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { MONGO_URL } = require('./config');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const logoutRouter = require('./controllers/logout');
+const perfilUserRouter = require('./controllers/perfilUser');
+const UploadRouter = require('./controllers/uploadProfileUser');
+const { usertExtractor } = require('./midlewares/auth');
 
-
-    // conexion base de datos
-    (async () => {
-        try {
-            await mongoose.connect(MONGO_URL);
-            console.log('conectado a la base de datos')
-        
-        } catch (error) {
-            console.log(error)
-        }
-    })();
+// conexion base de datos
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('conectado a la base de datos');
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 
@@ -29,5 +34,10 @@ app.use(cookieParser());
 // app.use('/api/users', require('./routes/users'))
 // app.use('/api/orders', require('./routes/orders'))
 
-module.exports = app;
+app.use('/api/registration', usersRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/logout', logoutRouter);
+app.use('/api/profile', perfilUserRouter)
+app.use('/api/upload', usertExtractor,UploadRouter )
 
+module.exports = app;
