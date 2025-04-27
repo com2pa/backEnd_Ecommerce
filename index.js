39124ebeb2d8c52177b084f6496871d56ffbc3c9
merwil vegas <com2pa@gmail.com>
2025-04-27 13:55:20 -0400

    se añadio registo de log de eventos del sistema

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const app = require('./app');
-
+const systemLogger = require('./help/system/systemLogger');
 const server = http.createServer(app);
-server.listen(3000, () => {
+server.listen(3000, async () => {
   console.log('servidor corriendo en el puerto 3000');
+  await systemLogger.logSystemEvent('system_start', {
+    version: process.env.npm_package_version,
+    nodeVersion: process.version,
+    platform: process.platform,
+  });
 });
 
 module.exports = server;
