08d49789b9f4fce45de9a6fab76a82162cbee16d
merwil vegas <com2pa@gmail.com>
2025-05-14 05:40:02 -0400

    se comento  sistema de evento del inicio del servicio

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const app = require('./app');
 const systemLogger = require('./help/system/systemLogger');
+const mongoose = require('mongoose');
+
 const server = http.createServer(app);
 server.listen(3000, async () => {
   console.log('servidor corriendo en el puerto 3000');
-  await systemLogger.logSystemEvent('system_start', {
-    version: process.env.npm_package_version,
-    nodeVersion: process.version,
-    platform: process.platform,
-  });
+  // await systemLogger.logSystemEvent('system_start', {
+  //   version: process.env.npm_package_version,
+  //   nodeVersion: process.version,
+  //   platform: process.platform,
+  // });
 });
 
 module.exports = server;
